297baace960fc811af4015f869bfb1cbd22089df
Anselem <anselemodims@gmail.com>
2022-02-03 15:19:45 +0100

    include the close button

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ window.addEventListener('DOMContentLoaded', () => {
   const btn = document.querySelector('#menu_btn');
 const menu = document.querySelector('#mobile-nav');
   btn.addEventListener('click', () => {
-    menu.style.display = 'block';
+    menu.style.display = 'flex';
   })
 })
