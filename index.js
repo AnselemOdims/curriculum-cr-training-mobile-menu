80c4af2e726cc499f803c81e3644726e9745e4c4
Anselem <anselemodims@gmail.com>
2022-02-03 15:21:49 +0100

    implement the close functionality

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 window.addEventListener('DOMContentLoaded', () => {
   const btn = document.querySelector('#menu_btn');
-const menu = document.querySelector('#mobile-nav');
+  const menu = document.querySelector('#mobile-nav');
+  const close = document.querySelector('#mobile-nav span');
   btn.addEventListener('click', () => {
-    menu.style.display = 'flex';
+      menu.style.display = 'flex';
   })
+  close.addEventListener('click', () => {
+    menu.style.display = 'none';
+})
 })
